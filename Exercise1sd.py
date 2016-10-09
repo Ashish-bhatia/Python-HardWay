812ab495f8f6506ce0d4d85d9655d743a2f678b4
Ashish Bhatia <inspiremeashish@gmail.com>
2016-10-09 15:20:51 +0800

    Add files via upload

---
+++ Exercise1sd.py
@@ -0,0 +1,22 @@
+# Exercise 1. - Study Drill
+#For this exercise, try these things:
+
+#  1) Make your script print another line.
+#  2) Make your script print only one of the lines.
+#  3) Put a # (octothorpe) character at the beginning of a line. What did it do? 
+#     Try to find out what this character does.
+
+
+#print ("Hello World!") 
+#print ("Hello Again") 
+#print ("I like typing this.")
+#print ("This is fun.") 
+#print ('Yay! Printing.')  
+#print ("I'd much rather you 'not'.") 
+#print ('I "said" do not touch this.')
+print('Hello I am really learning python')
+
+# Solution :
+# 1) added new print line.
+# 2) use '#' character to comment the lines.
+# 3) # character comments out the line it will not be executed.
