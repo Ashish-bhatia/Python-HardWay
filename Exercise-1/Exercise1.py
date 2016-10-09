ac72a3353e0906cbeba16d9fba9c610e04dbd288
Ashish Bhatia <inspiremeashish@gmail.com>
2016-10-09 15:22:19 +0800

    Add files via upload

---
+++ Exercise-1/Exercise1.py
@@ -0,0 +1,18 @@
+# Exercise 1. 
+# some additional details. 
+# In new version on python print function required curly braces.
+# this # symbol used to specify the comments.
+# String in python can be defined using double quotes (") or single Quote(')
+
+
+print ("Hello World!") # String defined using Double Quotes
+print ("Hello Again") 
+print ("I like typing this.")
+print ("This is fun.") 
+print ('Yay! Printing.')  # String defined using single quotes
+print ("I'd much rather you 'not'.") # string defined using mix
+print ('I "said" do not touch this.') # string defined using mix
+
+# with the last two lines you can easily think we can start using double quotes
+# and can use single quotes in between.  
+
